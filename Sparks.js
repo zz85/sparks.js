cbf0484786708e2998312655c3ac4f7eb0717c40
zz85 <zz85nus@gmail.com>
2011-12-16 06:33:20 +0800

    Added ShotCounter for single use trigger usage

--- Sparks.js
+++ Sparks.js
@@ -233,6 +233,26 @@ SPARKS.SteadyCounter.prototype.updateEmitter = function(emitter, time) {
 };
 
 
+/*
+ * Shot Counter produces specified particles 
+ * on a single impluse or burst
+ */
+
+SPARKS.ShotCounter = function(particles) {
+	this.particles = particles;
+	this.used = false;
+};
+
+SPARKS.ShotCounter.prototype.updateEmitter = function(emitter, time) {
+
+	if (this.used) {
+		return 0;
+	} else {
+		this.used = true;
+	}
+	
+	return this.particles;
+};
 
 
 /********************************
